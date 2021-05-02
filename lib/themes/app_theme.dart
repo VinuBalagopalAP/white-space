8051395a27ca59b8548d4cbdb78b37acddf7d1cd
vinubalagopalap <48175345+VinuBalagopalAP@users.noreply.github.com>
2021-05-02 17:53:28 +0530

    Added app_theme

---
+++ lib/themes/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:light_dark_theme_login_page/themes/dark_theme.dart';
+import 'light_theme.dart';
+
+class AppTheme {
+  static ThemeData light = lightTheme;
+  static ThemeData dark = darkTheme;
+}
