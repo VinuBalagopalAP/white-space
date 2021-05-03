08c9624643c633b33f29aa5831a70750d3d8c673
vinubalagopalap <48175345+VinuBalagopalAP@users.noreply.github.com>
2021-05-03 15:33:40 +0530

    Delete lib/themes directory

--- lib/themes/app_theme.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:light_dark_theme_login_page/themes/dark_theme.dart';
-import 'light_theme.dart';
-
-class AppTheme {
-  static ThemeData light = lightTheme;
-  static ThemeData dark = darkTheme;
-}
